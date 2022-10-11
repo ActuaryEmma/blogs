3c7c0e62a000a65ada88b43ed9af1b4a541d310e
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-10-12 01:16:54 +0300

    add copyright

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -23,11 +23,11 @@ function Footer() {
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL
+              CampKe
               <i class='fab fa-typo3' />
             </Link>
           </div>
-          <small class='website-rights'>TRVL © 2020</small>
+          <small class='website-rights'>CampKe © 2022</small>
           <div class='social-icons'>
             <Link
               class='social-icon-link facebook'
