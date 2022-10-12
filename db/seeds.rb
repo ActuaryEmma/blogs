31780b1388b21476f2702cdb45403670e7344922
Simon kamau <simon.kamau@student.moringaschool.com>
2022-10-12 09:37:29 +0300

    made changes and created user instance

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+#user instance
+User.create(name: "Mike Butler", email: "mike@example.com", password: "someone")
+User.create(name: "Kamau Mwangi", email: "kamaus@example.com", password: "manabadman")
+User.create(name: "Doreen Garcia", email: "dorgarcia@example.com", password: "dobetter")
+
+#comm
