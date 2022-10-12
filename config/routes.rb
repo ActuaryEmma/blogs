341534d4c40ed995c9a48a158b3dadb39d8e2cfb
Simon kamau <simon.kamau@student.moringaschool.com>
2022-10-12 13:51:51 +0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   
 
 
-  resources :comments
+  resources :comments, only: [:index, :create, :update, :destroy, :show]
   resources :blogs, only: [:index]
-  resources :users
+  resources :users, only: [:create]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
 
